2c7c45bc37f7ea147be6c180961645f2ad3e8667
hamza <seyedi.hamza@gmail.com>
2024-01-15 02:01:16 +0000

    cns

--- console.py
+++ console.py
@@ -58,3 +58,118 @@ class HBNBCommand(cmd.Cmd):
         command = mtd + " " + cls_name + " " + obj_id + " " + att_val
         self.onecmd(command)
         return command
+
+    def dict_update(self, cls_name, obj_id, s_dict):
+        """Method to help update an instance based on his ID with a dictionary."""
+        sd = s_dict.replace("'", '"')
+        dic = json.loads(sd)
+        if not cls_name:
+            print("** class name missing **")
+        elif cls_name not in storage.classes():
+            print("** class doesn't exist **")
+        elif obj_id is None:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(cls_name, obj_id)
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                attributes = storage.attributes()[cls_name]
+                for attribute, value in dic.items():
+                    if attribute in attributes:
+                        value = attributes[attribute](value)
+                    setattr(storage.all()[key], attribute, value)
+                storage.all()[key].save()
+
+    def do_quit(self, user_input):
+        """Method Quit to exit the program."""
+        return True
+
+    def do_EOF(self, user_input):
+        """EOF signal to exit the program."""
+        print()
+        return True
+
+
+    def do_create(self, user_input):
+        """Method to creates a new instance of BaseModel."""
+        if user_input == "" or user_input is None:
+            print("** class name missing **")
+        elif user_input not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            b = storage.classes()[user_input]()
+            b.save()
+            print(b.id)
+
+    def do_show(self, user_input):
+        """Method that prints the string representation 
+        of an instance based on class name and ID.
+        """
+        if user_input == "" or user_input is None:
+            print("** class name missing **")
+        else:
+            strings = user_input.split(' ')
+            if strings[0] not in storage.classes():
+                print("** class doesn't exist **")
+            elif len(strings) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(strings[0], strings[1])
+                if key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    print(storage.all()[key])
+
+    def do_destroy(self, user_input):
+        """Method that deletes an instance based 
+        on the class name and ID.
+        """
+        if user_input == "" or user_input is None:
+            print("** class name missing **")
+        else:
+            strings = user_input.split(' ')
+            if strings[0] not in storage.classes():
+                print("** class doesn't exist **")
+            elif len(strings) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(strings[0], strings[1])
+                if key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    del storage.all()[key]
+                    storage.save()
+
+    def do_all(self, user_input):
+        """Prints all string representation of all instances 
+        based or not on the class name.
+        """
+        if user_input != "":
+            strings = user_input.split(' ')
+            if strings[0] not in storage.classes():
+                print("** class doesn't exist **")
+            else:
+                n_list = [str(obj) for key, obj in storage.all().items()
+                      if type(obj).__name__ == strings[0]]
+                print(n_list)
+        else:
+            newList = [str(obj) for key, obj in storage.all().items()]
+            print(newList)
+
+    def do_count(self, user_input):
+        """Method that Retrieve the number of instances of 
+        a given class.
+        """
+        strings = user_input.split(' ')
+        if not strings[0]:
+            print("** class name missing **")
+        elif strings[0] not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            matches = [
+                k for k in storage.all() if k.startswith(
+                    strings[0] + '.')]
+            print(len(matches))
+
+
