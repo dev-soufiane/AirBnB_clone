b2bb6edd631ffda4c2652b92b69dce2e5c6ed95f
hamza <seyedi.hamza@gmail.com>
2024-01-17 00:38:57 +0000

    test

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,47 +1,45 @@
 #!/usr/bin/python3
-"""Unittest module for the Place Class."""
+"""Defines the unittest module for the amenity class"""
 
 import unittest
-from datetime import datetime
-import time
+import os
+import json
 from models.place import Place
 import re
-import json
-from models.engine.file_storage import FileStorage
-import os
 from models import storage
 from models.base_model import BaseModel
+from datetime import datetime
+import time
+from models.engine.file_storage import FileStorage
 
 
 class TestPlace(unittest.TestCase):
-
-    """Test Cases for the Place class."""
+    """Class test for place class."""
 
     def setUp(self):
-        """Sets up test methods."""
+        """Method to set up test."""
         pass
 
     def tearDown(self):
-        """Tears down test methods."""
+        """Method to tear down."""
         self.resetStorage()
         pass
 
     def resetStorage(self):
-        """Resets FileStorage data."""
+        """Methods to reset the filestorage."""
         FileStorage._FileStorage__objects = {}
         if os.path.isfile(FileStorage._FileStorage__file_path):
             os.remove(FileStorage._FileStorage__file_path)
 
     def test_8_instantiation(self):
-        """Tests instantiation of Place class."""
-
+        """Method to check instances of palce class."""
         b = Place()
         self.assertEqual(str(type(b)), "<class 'models.place.Place'>")
         self.assertIsInstance(b, Place)
         self.assertTrue(issubclass(type(b), BaseModel))
 
     def test_8_attributes(self):
-        """Tests the attributes of Place class."""
+        """Method to check attributes of place class."""
         attributes = storage.attributes()["Place"]
         o = Place()
         for k, v in attributes.items():
